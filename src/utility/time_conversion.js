988d1d5228bba2702e298c73ee699e6ae99ff6a4
John Hamm <johndhamm1@gmail.com>
2019-06-12 14:42:15 -0500

    tests for time_conversion

--- src/utility/time_conversion.js
+++ src/utility/time_conversion.js
@@ -1,10 +1,7 @@
 const TimeConversion = (ms) => {
   const hours = Math.floor( ms / 1000 / 60 / 60 );
-  // console.log("hours", hours);
   const minutes = Math.floor(ms / 1000 / 60) - ( hours * 60 );
-  // console.log("minutes", minutes);
   const seconds = (( ms / 1000) - (hours * 60 * 60) - (minutes * 60)).toFixed(1);
-  // console.log("seconds", seconds);
   let MM = minutes.toString();
   let SS = seconds.toString();
   if (seconds < 10) {
